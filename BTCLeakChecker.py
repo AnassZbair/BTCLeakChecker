c88e4377ce5626fe2ec2a417cbea0d85841f1424
Anass ZBAIR <65103469+Anassyou@users.noreply.github.com>
2021-01-26 16:32:16 +0100

    Update BTCLeakChecker.py

--- BTCLeakChecker.py
+++ BTCLeakChecker.py
@@ -8,7 +8,9 @@ for line in Lines:
     data = {'address': {line}}
     response = requests.post('https://allprivatekeys.com/check_address.php', data)
     if response.text.__contains__("Congratulation!"):
-        print colored("Not Leaked", "green")
+        print colored("Not Leaked ", "green")
+    elif response.text.__contains__("DANGER:"):
+        print colored("Leaked !", "red")
     else:
-        print colored("Leaked", "red")
+        print colored("Wrong BTC address !", "yellow")
 file1.close()
